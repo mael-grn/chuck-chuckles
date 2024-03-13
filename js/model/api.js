0511c6f72a83065b0d3d43681cf871300e1c9d60
garnmael <maelgarnier5@gmail.com>
2024-03-13 15:32:04 +0100

    model api

--- js/model/api.js
+++ js/model/api.js
@@ -5,11 +5,52 @@
 class Api {
 
     //url pour faire les requêtes
-    #urlReq = "https://api.chucknorris.io/jokes/search?query=";
+    #urlReq;
 
     //valeur de la recherche
-    #query
+    #query;
 
+    /**
+     * constructeur de l'objet API
+     */
+    constructor() {
+        this.#urlReq = "https://api.chucknorris.io/jokes/search?query="
+    }
     
+    /**
+     * permet de rechercher des blagues via un appel à l'api
+     * @param {*} query la recherche saisie par l'utilisateur
+     */
+    async searchJokes(query) {
+        /*
+        try {
+            // Construire l'URL complète en ajoutant la requête à l'URL de base
+            const url = chuckNorrisAPIURL + encodeURIComponent(query);
+            
+            // Effectuer la requête avec fetch() et attendre la réponse
+            const response = await fetch(url);
+    
+            // Vérifier si la réponse est réussie (code 200)
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            // Convertir la réponse JSON et attendre le résultat
+            const data = await response.json();
+            
+            // Traiter les données JSON ici
+            console.log(data);
+            
+            // Retourner les données si nécessaire
+            return data;
+        } catch (error) {
+            // Gérer les erreurs
+            console.error('There was a problem with the fetch operation:', error);
+            // Vous pouvez choisir de retourner une valeur par défaut ou de relancer l'erreur
+            throw error;
+        }
+        */
+    }
+
 
 }
