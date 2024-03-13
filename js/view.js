be6f6d4c1acd8c8129d0d43d0002cee7d3dfd09b
garnmael <maelgarnier5@gmail.com>
2024-03-13 14:50:35 +0100

    ajout fichiers code js

---
+++ js/view.js
@@ -0,0 +1,8 @@
+/**
+ * Objet constant représentant la vue.
+ */
+const view = {
+    // Bouton pour lancer la recherche
+    btnRecherche: document.getElementById("btn-lancer-recherche"),
+  
+  };
