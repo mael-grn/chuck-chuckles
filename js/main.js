f7d60893f9df1dee6d313255bde48183201fa118
maelg <mael.garnier@univ-grenoble-alpes.fr>
2024-03-14 13:25:05 +0100

    commentaire

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ view.btnRecherche.addEventListener("click", () => {
     var inputValue = document.getElementById('input-text-response').value;
     console.log("input value : " + inputValue);
 
+    //on effectue le recherche et on mets le resultat dans la console
     api.searchJoke(inputValue).then(jokes => {
         console.log(jokes);
     });
