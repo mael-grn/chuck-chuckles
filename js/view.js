d0f91acaa0575d1d7229d8cca0668455dd74f6a2
maelg <mael.garnier@univ-grenoble-alpes.fr>
2024-03-19 13:37:05 +0100

    correction bug chargement

--- js/view.js
+++ js/view.js
@@ -6,7 +6,7 @@ const view = {
     btnRecherche: document.getElementById("btn-lancer-recherche"),
     inputRecherche: document.getElementById("input-text-response"),
     blocResultat: document.getElementById("bloc-resultats"),
-    loadingImage: document.getElementsByClassName("loading"),
+    loadingImage: document.querySelector(".loading"),
   };
 
 export default view;
